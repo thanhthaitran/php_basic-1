b6bef4a4924826f3efca6896dd211aa076c23f69
Thai.Tran <thai.tran@asiantech.vn>
2018-04-13 15:05:22 +0700

    show list users

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,16 @@
 <?php
-  $servername = "localhost";
-  $username = "root";
-  $password = "root";
-  $namedb = "php_basic";
-  try {
-    $conn = new PDO("mysql:host=$servername;dbname=$namedb", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-  }
-  catch(PDOException $e){
-    echo "Connection failed: " . $e->getMessage();
+  function connect(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $namedb = "php_basic";
+    try {
+      $conn = new PDO("mysql:host=$servername;dbname=$namedb", $username, $password);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $conn; 
+    }
+    catch(PDOException $e){
+      echo "Connection failed: " . $e->getMessage();
+    }
   }
 ?>
---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title> Responsive</title>
+  <link rel="stylesheet" href="style.css">
+  <?php
+    include("dbconnect.php");
+    $conn = connect();
+  ?>
+</head>
+<body>
+  <h1><span class="blue"><span class="yellow">Show list users</pan></h1>  
+  <div class="add-user">
+    <a href="">New user</a>
+  </div>
+  <?php
+    $stmt = $conn->prepare("select * from users");
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+  ?>
+  <table class="container">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Full Name</th>
+        <th>User Name</th>
+        <th>Email</th>
+        <th>Phone</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+        foreach($result as $key => $values){
+          $id = $values['id'] ;
+          $full_name =$values['fullname'];
+          $user_name =$values['username'];
+          $email = $values['email'];
+          $phone = $values['phone'];
+      ?>
+      <tr>
+        <td><?php echo $id ?></td>
+        <td><?php echo $full_name ?></td>
+        <td><?php echo $user_name ?></td>
+        <td><?php echo $email ?></td>
+        <td><?php echo $phone ?></td>
+        <td>
+          <a href="edit-user.html"><i class="fas fa-edit"></i></a> |
+          <a href="delete-user.html"><i class="fas fa-trash-alt"></i></a>
+        </td>
+      </tr>
+    </tbody>
+    <?php } ?>
+  </table>
+  <style>
+    .add-user {
+      width: 80%;
+      margin: 0 auto; 
+    }
+    .add-user a{
+      background-color: #323C50;
+      text-decoration: none;
+      color: aliceblue;
+      padding: 10px;
+      font-weight: normal;
+      font-size: 1em;
+      -webkit-box-shadow: 0 2px 2px -2px #0E1119;
+        -moz-box-shadow: 0 2px 2px -2px #0E1119;
+              box-shadow: 0 2px 2px -2px #0E1119;
+    }
+    .add-user a:hover {
+      background-color: #FFF842;
+      color: #403E10;
+      font-weight: bold;
+      box-shadow: #7F7C21 -1px 1px, #7F7C21 -2px 2px, #7F7C21 -3px 3px, #7F7C21 -4px 4px, #7F7C21 -5px 5px, #7F7C21 -6px 6px;
+      transform: translate3d(6px, -6px, 0);
+      transition-delay: 0s;
+      transition-duration: 0.4s;
+      transition-property: all;
+      transition-timing-function: line;
+    }
+  </style>
+  <script defer src="/static/fontawesome/fontawesome-all.js"></script>
+  <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
+</body>
+</html>
